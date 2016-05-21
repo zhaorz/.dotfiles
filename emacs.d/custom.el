322e7b8acba16d2ab8f08f0072afdebb1f09fd38
Richard Zhao <richardz@andrew.cmu.edu>
2016-05-21 18:02:56 -0400

    Refactor init.el with use-package

---
+++ emacs.d/custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(linum-format (quote dynamic))
+ '(org-agenda-files (quote ("~/tmp/notes.org")))
+ '(org-hide-leading-stars t)
+ '(powerline-display-hud nil)
+ '(powerline-height 22)
+ '(powerline-text-scale-factor nil)
+ '(sml/extra-filler 0)
+ '(sml/mode-width (quote full))
+ '(sml/name-width 25)
+ '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes)))
+ '(sml/shorten-modes t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:background "#eee8d5" :foreground "#93a1a1"))))
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "Source Code Pro"))))
+ '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 1.2 :family "Source Code Pro"))))
+ '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.15 :family "Source Code Pro"))))
+ '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 1.1 :family "Source Code Pro"))))
+ '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :family "Source Code Pro"))))
+ '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :family "Source Code Pro"))))
+ '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :family "Source Code Pro"))))
+ '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :family "Source Code Pro"))))
+ '(sml/modified ((t (:foreground "#b58900" :weight bold)))))
