3f188b18153ff60d77b80adf6dbdfdf465d1696c
Richard Zhao <richard@rzhao.io>
2016-05-21 18:02:56 -0400

    Refactor init.el with use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,45 +1,8 @@
-;; init.el
-;;
-;; Richard Zhao
-;;
+;;; init.el
+;;;
+;;; Richard Zhao
+;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(linum-format (quote dynamic))
- '(org-agenda-files (quote ("~/tmp/notes.org")))
- '(org-hide-leading-stars t)
- '(powerline-display-hud nil)
- '(powerline-height 22)
- '(powerline-text-scale-factor nil)
- '(sml/extra-filler 0)
- '(sml/mode-width (quote full))
- '(sml/name-width 25)
- '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes)))
- '(sml/shorten-modes t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(linum ((t (:background "#eee8d5" :foreground "#93a1a1"))))
- '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "Source Code Pro"))))
- '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 1.2 :family "Source Code Pro"))))
- '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.15 :family "Source Code Pro"))))
- '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 1.1 :family "Source Code Pro"))))
- '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :family "Source Code Pro"))))
- '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :family "Source Code Pro"))))
- '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :family "Source Code Pro"))))
- '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :family "Source Code Pro"))))
- '(sml/modified ((t (:foreground "#b58900" :weight bold)))))
-
-(require 'cl)
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -47,15 +10,73 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-;; Vim text editing
-(setq evil-want-C-i-jump nil)
-(require 'evil)
-(evil-mode t)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-;; tramp
-(require 'tramp)
-(setq tramp-default-method "ssh")
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
 
+(use-package evil
+  :init
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode t)
+  (evil-set-initial-state 'term-mode 'emacs)
+
+  (define-key evil-normal-state-map (kbd "C-k") (lambda ()
+                                                  (interactive)
+                                                  (evil-scroll-up 15)
+                                                  ))
+  (define-key evil-normal-state-map (kbd "C-j") (lambda ()
+                                                  (interactive)
+                                                  (evil-scroll-down 15)
+                                                  ))
+  (define-key evil-normal-state-map (kbd "C-n") 'next-line)
+  (define-key evil-normal-state-map (kbd "C-p") 'previous-line)
+  (define-key evil-normal-state-map (kbd ";") 'evil-ex))
+
+(use-package helm
+  :diminish helm-mode
+  :init
+  (progn
+    (require 'helm-config)
+    ;; rebind tab to run persistent action
+    (setq helm-split-window-in-side-p           t
+          helm-move-to-line-cycle-in-source     t
+          helm-ff-search-library-in-sexp        t
+          helm-scroll-amount                    8
+          helm-ff-file-name-history-use-recentf t)
+
+    (helm-mode 1)
+    )
+  :bind (
+         ("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-mini)
+         :map evil-ex-map
+         ("e" . helm-find-files)
+         ("b" . helm-buffers-list)
+         )
+  )
+
+(use-package multi-term
+  :bind (([f9] . multi-term))
+  :config
+  (setq multi-term-program "/bin/bash")
+  (add-hook 'term-mode-hook
+            (lambda ()
+              (setq term-buffer-maximum-size 4096)))
+  (add-hook 'term-mode-hook
+            (lambda ()
+              (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+              (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
+  )
+
+(use-package tramp
+  :defer t
+  :config
+  (setq tramp-default-method "ssh"))
 
 ;; Globals
 (setq inhibit-startup-message t)
@@ -65,116 +86,51 @@
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 
-;; Shell
-(require 'multi-term)
-(setq multi-term-program "/bin/bash")
-(evil-set-initial-state 'term-mode 'emacs)   ;; keep term in emacs mode
-(add-hook 'term-mode-hook
-          (lambda ()
-            (setq term-buffer-maximum-size 4096)))
-(add-hook 'term-mode-hook
-          (lambda ()
-            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
-            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
-
 (setq explicit-shell-file-name "/bin/bash")
 (setq shell-file-name "bash")
-(global-set-key [f9] 'multi-term)
 
 ;; Visual
 (load-theme 'solarized-light t)
 (add-to-list 'default-frame-alist
              '(font . "Source Code Pro 11"))
 (setq solarized-use-less-bold t)
-(setq ns-use-srgb-colorspace nil)
+(setq ns-use-srgb-colorspace nil) ; fix weird colors in powerline
 
-;; No lines over 80 cols
-(require 'whitespace)
-(setq whitespace-line-column 80) ;; limit line length
-(setq whitespace-style '(face lines-tail))
-(add-hook 'prog-mode-hook 'whitespace-mode)
+(use-package whitespace
+  :diminish whitespace-mode
+  :config
+  (setq whitespace-line-column 80) ; limit line length
+  (setq whitespace-style '(face lines-tail))
+  (add-hook 'prog-mode-hook 'whitespace-mode)
+  )
 
+(use-package company
+  :diminish company-mode
+  :ensure t
+  :defer 2
+  :config
+  (setq company-idle-delay 0.3)
+  (global-company-mode))
 
+(add-hook 'prog-mode-hook 'turn-on-auto-fill)
 
-;; Autocompletion
-(setq company-idle-delay 0.3)
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package key-chord
+  :config
+  (setq key-chord-two-keys-delay 0.2)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+  (key-chord-mode 1)
+  )
 
-(require 'key-chord)
-(setq key-chord-two-keys-delay 0.2)
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-(key-chord-mode 1)
-(define-key evil-normal-state-map (kbd ";") 'evil-ex)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(require 'better-defaults)
-
-(require 'helm)
-(require 'helm-config)
-
-;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-
-;; rebind tab to run persistent action
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-z")  'helm-select-action)
-
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-
-(setq helm-split-window-in-side-p           t
-      helm-move-to-line-cycle-in-source     t
-      helm-ff-search-library-in-sexp        t
-      helm-scroll-amount                    8
-      helm-ff-file-name-history-use-recentf t)
-
-(helm-mode 1)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x b") 'helm-mini)
-
-(define-key evil-ex-map "e " 'helm-find-files)
-(define-key evil-ex-map "b " 'helm-buffers-list)
-
-;; Never override C-n/C-p
-(define-key evil-normal-state-map (kbd "C-n") 'next-line)
-(define-key evil-normal-state-map (kbd "C-p") 'previous-line)
-
-;; Scroll with C-j/C-k
-(define-key evil-normal-state-map (kbd "C-k") (lambda ()
-                                                (interactive)
-                                                (evil-scroll-up 15)
-                                                ))
-(define-key evil-normal-state-map (kbd "C-j") (lambda ()
-                                                (interactive)
-                                                (evil-scroll-down 15)
-                                                ))
-
-;; Switch windows faster
-(windmove-default-keybindings)
 (global-set-key (kbd "M-h") 'evil-window-left)
 (global-set-key (kbd "M-j") 'evil-window-down)
 (global-set-key (kbd "M-k") 'evil-window-up)
 (global-set-key (kbd "M-l") 'evil-window-right)
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; diminish
-(when (require 'diminish nil 'noerror)
-  (diminish 'undo-tree-mode)
-  (diminish 'whitespace-mode)
-  (eval-after-load "company"
-      '(diminish 'company-mode))
-  (eval-after-load "helm"
-      '(diminish 'helm-mode))
-  (eval-after-load "helm-config"
-      '(diminish 'helm-mode))
-  )
+(diminish 'undo-tree-mode)
 
 (add-hook 'emacs-lisp-mode-hook
   (lambda()
@@ -206,7 +162,9 @@
                     )
 
 ;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
+(use-package magit
+  :bind (("C-x g" . magit-status))
+  )
 
 (defun powerline-custom-theme ()
   "Setup the default mode-line."
@@ -227,7 +185,7 @@
                           (lhs (list
                                 (if (buffer-modified-p)
                                     (powerline-raw "%*" '(:foreground "#b58900") 'l)
-                                (powerline-raw "%*" nil 'l))
+                                  (powerline-raw "%*" nil 'l))
                                 (when powerline-display-buffer-size
                                   (powerline-buffer-size nil 'l))
                                 (when powerline-display-mule-info
@@ -268,6 +226,10 @@
                              (powerline-fill face2 (powerline-width rhs))
                              (powerline-render rhs)))))))
 
-(require 'powerline)
-(powerline-custom-theme)
-(setq powerline-default-separator 'slant)       ;; mirrored diagonals
+(use-package powerline
+  :config
+  (powerline-custom-theme)
+  (setq powerline-default-separator 'slant))
+
+(provide 'init)
+;;; init.el ends here
