19382ada2dcf05f8dbb8ef88f7ce12b2c6e3b3b9
Richard Zhao <richard@rzhao.io>
2016-05-27 17:28:51 -0400

    Add evil-leader, brew bin to path, clean autosave

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-mode-text " ")
  '(custom-safe-themes
    (quote
     ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
