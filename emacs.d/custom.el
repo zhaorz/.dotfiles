16ba61c86e7909b46af7c8144bc025a8ecd80b8c
Richard Zhao <richard@rzhao.io>
2016-12-01 19:20:31 -0500

    Add lua mode

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -12,7 +12,7 @@
  '(org-hide-leading-stars t)
  '(package-selected-packages
    (quote
-    (company-ghc ghc haskell-mode slim-mode magit powerline hydra markdown-mode org-bullets zenburn-theme solarized-theme company-web web-mode emmet-mode key-chord flycheck yasnippet company multi-term helm projectile evil exec-path-from-shell use-package)))
+    (lua-mode sml-mode company-ghc ghc haskell-mode slim-mode magit powerline hydra markdown-mode org-bullets zenburn-theme solarized-theme company-web web-mode emmet-mode key-chord flycheck yasnippet company multi-term helm projectile evil exec-path-from-shell use-package)))
  '(powerline-display-hud nil)
  '(powerline-height 18)
  '(powerline-text-scale-factor nil)
